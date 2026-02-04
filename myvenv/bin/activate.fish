df5d7292105abb7fc4f8abcf00abd7c78b204f12
NeuroMedAI <hello@neuromedai.org>
2026-02-05 06:31:08 +0800

    updated

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/julia/Downloads/med_ai-2/myvenv"
+set -gx VIRTUAL_ENV "/Users/julia/Downloads/med_ai-3/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
